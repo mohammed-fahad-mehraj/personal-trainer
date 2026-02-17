98714f9ea50f01c5b2c723798c2fe37e84432453
Mohammed Fahad Mehraj <your_email@example.com>
2026-02-17 02:23:53 -0600

    fix: routing issue

--- server/index.ts
+++ server/index.ts
@@ -106,6 +106,6 @@ import path from "path";
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
